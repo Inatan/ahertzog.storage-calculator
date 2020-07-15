987a8dc4283ec316d7828c9b1e4d204c6925907c
Inatan <inatan_hertzog@hotmail.com>
2020-07-14 21:29:33 -0300

    first commit

---
+++ store-calculator/Models/MateriaPrima.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace store_calculator.Models
+{
+    public class MateriaPrima
+    {
+        public string Nome { get; set; }
+        public int Medida { get; set; }
+        public float ValorUnitario { get; set; }
+
+        public MateriaPrima()
+        {
+            this.Nome = String.Empty;
+            this.Medida = 0;
+            this.ValorUnitario = 0.00f;
+        }
+    }
+}
