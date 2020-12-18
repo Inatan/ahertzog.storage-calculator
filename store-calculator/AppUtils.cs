b3c50b8c84716ed28f068c6251bd07ddb0e33bc4
inatan <inatan_hertzog@hotmail.com>
2020-12-18 08:01:59 -0300

    format currency pt-br

--- store-calculator/AppUtils.cs
+++ store-calculator/AppUtils.cs
@@ -1,9 +1,22 @@
-﻿using System.Windows;
+﻿using System.Globalization;
+using System.Threading;
+using System.Windows;
 
 namespace Store.Calculator.App
 {
     public class AppUtils
     {
+        public static CultureInfo cultureInfo
+        {
+            get
+            {
+                CultureInfo cultureInfo = new CultureInfo("pt-BR");
+                cultureInfo.NumberFormat.CurrencySymbol = "";
+                Thread.CurrentThread.CurrentCulture = cultureInfo;
+                return cultureInfo;
+            }
+        }
+
         public static void MensagemErro(string message, string title = "Erro")
         {
             MessageBox.Show(message, title, MessageBoxButton.OK, MessageBoxImage.Error);
--- store-calculator/Views/Orcamento.xaml.cs
+++ store-calculator/Views/Orcamento.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Store.Calculator.Services.Handlers;
 using System;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Media;
@@ -13,15 +15,20 @@ namespace Store.Calculator.App.Views
     {
         private readonly ICadastroMaterialHandler _handler;
 
+        private decimal totalPayment;
+
         public Orcamento(ICadastroMaterialHandler handler)
         {
             _handler = handler;
+            totalPayment = 0.00M;
             InitializeComponent();
         }
 
         private void AtualizaTabela(string nome, string unidade, string preco, string quantidade, string total )
         {
             TableRow tableRow = new TableRow();
+            if (TableRowValor.Rows.Count > 1)
+                TableRowValor.Rows.RemoveAt(TableRowValor.Rows.Count - 1);
             TableRowValor.Rows.Add(tableRow);
             if(TableRowValor.Rows.Count % 2 == 1)
                 tableRow.Background = Brushes.LightGray;
@@ -32,12 +39,20 @@ namespace Store.Calculator.App.Views
             tableRow.Cells.Add(new TableCell(new Paragraph(new Run(quantidade))));
             tableRow.Cells.Add(new TableCell(new Paragraph(new Run(total))));
 
-            decimal totalTable = Convert.ToDecimal(total);
-            if (FooterText.Text != string.Empty)
-            {
-                totalTable = Convert.ToDecimal(FooterText.Text.Replace(",", ".").Replace("Total: ", "")) + totalTable;
-            }
-            FooterText.Text = $"Total: {totalTable}";
+            tableRow = new TableRow();
+            decimal totalCel= Convert.ToDecimal(total, AppUtils.cultureInfo);
+            totalPayment += totalCel;
+
+            string totalTable = $"Total: R$ {String.Format(AppUtils.cultureInfo, "{0:C}", totalPayment)}";
+            Paragraph paragraphFooter = new Paragraph(new Run(totalTable));
+            paragraphFooter.FontSize = 20;
+            paragraphFooter.TextAlignment = TextAlignment.Right;
+            paragraphFooter.FontWeight = FontWeights.Bold;
+            TableCell tableCellFooter = new TableCell(paragraphFooter);
+            tableCellFooter.ColumnSpan = 5;
+            tableRow.Cells.Add(tableCellFooter);
+            TableRowValor.Rows.Add(tableRow);
+
         }
 
         private void BtnAdicionarProduto_Click(object sender, RoutedEventArgs e)
